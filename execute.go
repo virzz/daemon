4ebe42de5a8dd2ccbbeb6922ca4659975e154fbe
陌竹 <mozhu233@outlook.com>
2024-05-03 00:31:49 +0800

    ✨ Feat(*): Read config when not used

--- execute.go
+++ execute.go
@@ -13,18 +13,24 @@ type ActionFunc func(cmd *cobra.Command, args []string) error
 func Execute(action ActionFunc) {
 	viper.BindPFlags(rootCmd.PersistentFlags())
 	viper.BindPFlags(rootCmd.Flags())
-	rootCmd.PersistentPreRun = func(cmd *cobra.Command, args []string) {
-		InstanceTag = viper.GetString("instance")
-		if InstanceTag != "" {
-			viper.AddConfigPath(InstanceTag)
-		}
+	viper.AutomaticEnv()
+	if viper.ConfigFileUsed() == "" {
 		viper.AddConfigPath(".")
 		viper.AddConfigPath("$HOME/.config/" + rootCmd.Use)
 		viper.AddConfigPath("/etc/" + rootCmd.Use)
 		viper.SetConfigName(rootCmd.Use)
 		viper.SetConfigType("yaml")
-		viper.ReadInConfig()
-		viper.AutomaticEnv()
+		if err := viper.ReadInConfig(); err != nil {
+			vlog.Error(err.Error())
+			return
+		}
+	}
+	rootCmd.PersistentPreRunE = func(cmd *cobra.Command, args []string) error {
+		InstanceTag = viper.GetString("instance")
+		if InstanceTag != "" {
+			viper.AddConfigPath(InstanceTag)
+		}
+		return viper.ReadInConfig()
 	}
 	rootCmd.RunE = action
 	if err := rootCmd.Execute(); err != nil {
