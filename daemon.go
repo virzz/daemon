df9db850f1e950827345f09daafae0c6f40e4906
Mozhu <mozhu233@outlook.com>
2024-10-18 13:33:34 +0800

    Fix: remote

--- daemon.go
+++ daemon.go
@@ -8,10 +8,7 @@ import (
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
-	"github.com/virzz/vlog"
 )
-
-const defaultRemoteEndpoint = "config.app.virzz.com"
 
 var (
 	std     *Daemon
@@ -70,61 +67,4 @@ func Execute(action ActionFunc) {
 		fmt.Println("Error: ", err)
 		os.Exit(1)
 	}
-}
-
-func ExecuteE(action ActionFunc) error {
-	if std.logger == nil || std.systemd.logger == nil {
-		std.SetLogger(vlog.Log)
-	}
-	rootCmd.PreRunE = func(cmd *cobra.Command, args []string) (err error) {
-		instance, _ := cmd.PersistentFlags().GetString("instance")
-		config, _ := cmd.PersistentFlags().GetString("config")
-		viper.SetConfigType("json")
-		if config != "" {
-			viper.SetConfigFile(config)
-		} else {
-			viper.AddConfigPath(".")
-			viper.SetConfigName("config_" + instance)
-		}
-
-		configLoaded := false
-		if std.remoteConfig {
-			remoteEndpoint, _ := cmd.PersistentFlags().GetString("remote-endpoint")
-			if remoteEndpoint == "" {
-				remoteEndpoint = std.remoteEndpoint
-			}
-			if remoteEndpoint == "" {
-				remoteEndpoint = defaultRemoteEndpoint
-			}
-			key := fmt.Sprintf("/%s/%s/%s/%s", std.project, std.systemd.AppID, std.systemd.Version, instance)
-			err = viper.AddSecureRemoteProvider("virzz", remoteEndpoint, key, string(std.secretKey))
-			if err != nil {
-				std.logger.Warn("Failed to add remote config provider", "err", err.Error())
-			} else {
-				err = viper.ReadRemoteConfig()
-				if err != nil {
-					std.logger.Warn("Failed to load remote config", "err", err.Error())
-				} else {
-					configLoaded = true
-				}
-			}
-		}
-		if !configLoaded {
-			err = viper.ReadInConfig()
-			if err != nil {
-				return err
-			}
-		}
-		return nil
-	}
-	rootCmd.RunE = action
-	viper.BindPFlags(rootCmd.PersistentFlags())
-	viper.BindPFlags(rootCmd.Flags())
-	viper.SetEnvPrefix(rootCmd.Use)
-	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
-	viper.AutomaticEnv()
-	if err := rootCmd.Execute(); err != nil {
-		return err
-	}
-	return nil
 }
--- execute-remote.go
+++ execute-remote.go
@@ -9,13 +9,18 @@ import (
 	"crypto/sha256"
 	"crypto/x509"
 	"encoding/pem"
+	"fmt"
 	"io"
+	"strings"
 
 	"github.com/pkg/errors"
+	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+	"github.com/virzz/vlog"
 )
 
-const defaultPublicKey = `-----BEGIN PUBLIC KEY-----
+const (
+	defaultPublicKey = `-----BEGIN PUBLIC KEY-----
 MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAkhbTnK9POOy317u6ovxE
 UqFT5FUPaTSSmAa0gDepG7B1SpDHmpsarJlf//doy9A4bqysxQ8Fu1njtxXU861s
 J5lxS1p72UreuZoTbV+mnQFzeIqbPDiqQruzqws+hnKAVHdDcjy6NPvUH1na4bNf
@@ -24,6 +29,8 @@ BqvNT3/ayjtHlrYmnTOxGHv7H+j0JQ/yz2/ap7PWdIfspqGJZSV9iPKKfKfw37KF
 H19ekMDgL248Y4PiK5BqWD4jY1hQfMsQf2ZVs2g6gGNQPAYLMiAXA4ngNurA4Kz+
 xQIDAQAB
 -----END PUBLIC KEY-----`
+	defaultRemoteEndpoint = "config.app.virzz.com"
+)
 
 func EnableRemoteConfig(project string, publicKey ...string) error {
 	return std.EnableRemoteConfig(project, publicKey...)
@@ -58,4 +65,61 @@ func (d *Daemon) EnableRemoteConfig(project string, publicKey ...string) error {
 		return nil
 	}
 	return errors.New("not an RSA public key")
+}
+
+func ExecuteE(action ActionFunc) error {
+	if std.logger == nil || std.systemd.logger == nil {
+		std.SetLogger(vlog.Log)
+	}
+	rootCmd.PreRunE = func(cmd *cobra.Command, args []string) (err error) {
+		instance, _ := cmd.PersistentFlags().GetString("instance")
+		config, _ := cmd.PersistentFlags().GetString("config")
+		viper.SetConfigType("json")
+		if config != "" {
+			viper.SetConfigFile(config)
+		} else {
+			viper.AddConfigPath(".")
+			viper.SetConfigName("config_" + instance)
+		}
+
+		configLoaded := false
+		if std.remoteConfig {
+			remoteEndpoint, _ := cmd.PersistentFlags().GetString("remote-endpoint")
+			if remoteEndpoint == "" {
+				remoteEndpoint = std.remoteEndpoint
+			}
+			if remoteEndpoint == "" {
+				remoteEndpoint = defaultRemoteEndpoint
+			}
+			key := fmt.Sprintf("/%s/%s/%s/%s", std.project, std.systemd.AppID, std.systemd.Version, instance)
+			err = viper.AddSecureRemoteProvider("virzz", remoteEndpoint, key, string(std.secretKey))
+			if err != nil {
+				std.logger.Warn("Failed to add remote config provider", "err", err.Error())
+			} else {
+				err = viper.ReadRemoteConfig()
+				if err != nil {
+					std.logger.Warn("Failed to load remote config", "err", err.Error())
+				} else {
+					configLoaded = true
+				}
+			}
+		}
+		if !configLoaded {
+			err = viper.ReadInConfig()
+			if err != nil {
+				return err
+			}
+		}
+		return nil
+	}
+	rootCmd.RunE = action
+	viper.BindPFlags(rootCmd.PersistentFlags())
+	viper.BindPFlags(rootCmd.Flags())
+	viper.SetEnvPrefix(rootCmd.Use)
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+	viper.AutomaticEnv()
+	if err := rootCmd.Execute(); err != nil {
+		return err
+	}
+	return nil
 }
